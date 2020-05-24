136e2b135c8bd8a976821cad3367f22c76bfa391
Vidya Krishnan <55047084+anavidya@users.noreply.github.com>
2020-05-24 12:21:46 +0100

    Update jsscripts.js

--- jsscripts.js
+++ jsscripts.js
@@ -1,5 +1,6 @@
 <script>
-    $(".link").click(function(e) {
+$(".link").click(function(e) {
+    alert('clicked');
     e.preventDefault();
     $('.content div').fadeOut('slow');
     $('#' + $(this).data('rel')).fadeIn('slow');
