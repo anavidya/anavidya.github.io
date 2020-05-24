83feed4f8ef6f8e3df3c674cfe2a31e14cbeea57
Vidya Krishnan <55047084+anavidya@users.noreply.github.com>
2020-05-24 14:21:10 +0100

    Update jsscripts.js

--- jsscripts.js
+++ jsscripts.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
     $('#intro').show();
+    document.getElementById("robin").style.visibility = 'visible';
     $("a.link").click(function(e) {
     e.preventDefault();
     $('.content div').fadeOut('slow');
