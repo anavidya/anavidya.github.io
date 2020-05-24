8dcc7136296a2fcf00321748d806b05750020ce2
Vidya Krishnan <55047084+anavidya@users.noreply.github.com>
2020-05-24 19:20:51 +0100

    Update jsscripts.js

--- jsscripts.js
+++ jsscripts.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
     $('#intro').show();
     $('#robin').show();
     $("a.link").click(function(e) {
-    e.preventDefault();
-    
-    if $('#' + $(this).data('rel')) = 'NOSQLdb'{
-        $("#content div").load("NOSQLdb.html");
-    }
-    $('.content div').fadeOut('slow');
-    $('#' + $(this).data('rel')).fadeIn('slow');
+        e.preventDefault();
+
+        if $(this).data('rel') = 'NOSQLdb'{
+            $("#content div").load("NOSQLdb.html");
+        }
+        $('.content div').fadeOut('slow');
+        $('#' + $(this).data('rel')).fadeIn('slow');
     });
 });
