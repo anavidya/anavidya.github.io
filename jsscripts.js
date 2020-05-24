de563decc8001dd1eb65cfb6b6fcc1293657bb3a
Vidya Krishnan <55047084+anavidya@users.noreply.github.com>
2020-05-24 12:50:22 +0100

    Update jsscripts.js

--- jsscripts.js
+++ jsscripts.js
@@ -1,11 +1,12 @@
-$("a.link").click(function(e) {
+
+
+$(document).ready(function() {
+    $('a').click(function() {
+        alert('ho ho ho');
+    });
+    $("a.link").click(function(e) {
     e.preventDefault();
     $('.content div').fadeOut('slow');
     $('#' + $(this).data('rel')).fadeIn('slow');
-});
-
-$(document).ready(function() {
-        $('a').click(function() {
-            alert('ho ho ho');
-        });
-
+    });
+}
